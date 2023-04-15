a1557e33c480945c76876891f520dc6a131a59cb
ryanqle <ryan.q.le@gmail.com>
2023-04-15 00:38:22 -0500

    Order file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './pages/App/App'
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}",
-  "./src/**.{html,js,jsx}"],
+  "./src/**.{html,js,jsx}",
+  "./src/pages/App/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
