9b1b05c31a5ea0b1b99bd353dc2fd1599d3e8834
ryanqle <ryan.q.le@gmail.com>
2023-04-19 13:29:02 -0500

    Show Past Session

--- src/components/PastExercise/PastExercise.jsx
+++ src/components/PastExercise/PastExercise.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useState, useEffect, useRef } from 'react';
+import * as fitnessLogAPI from '../../utilities/fitnesslogs-api';
 
-export default function PastExercise() {
+export default function PastExercise({sessionId, user}) {
+
+  const [session, setSession] = useState({})
+
+  useEffect(function() {
+    async function showSession(){
+      const currentSession = await fitnessLogAPI.getSession(sessionId)
+      setSession(currentSession);
+    }
+    showSession();
+  },[])
+  console.log(session)
   return (
-    <div>PastExercise</div>
+    <>
+    <div>{session.sessionName}</div>
+    <div>{new Date(session.updatedAt).toLocaleDateString()}</div>
+    </>
   )
 }
