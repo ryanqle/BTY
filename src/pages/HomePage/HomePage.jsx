4cdcd674ba3cf3e31a610737a5b0565a1774f413
ryanqle <ryan.q.le@gmail.com>
2023-04-21 01:07:31 -0500

    Logo to homepage and favicon

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../../assets/LogoVector.svg';
 
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <img src={logo} alt="BTY logo" className="mb-8" />
       <h1 className="text-4xl font-bold mb-8 text-blue-500 text-2xl">Welcome to BTY</h1>
       <div className="w-2/3 text-xl mb-8 text-center">
         Introducing our cutting-edge fitness tracker designed to help you stay fit and healthy even while you're at your desk! With a range of advanced features including personalized calisthenic workout plans and progress monitoring, our tracker is the perfect tool to keep you motivated and on track towards achieving your fitness goals. Say goodbye to sedentary habits and hello to a healthier, more active lifestyle with our desk-friendly fitness tracker.
