3aa6a4078c68fb466d7cb622b009a4c9919f925c
ryanqle <ryan.q.le@gmail.com>
2023-04-21 01:09:38 -0500

    Update homepage description

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,7 +8,7 @@ export default function HomePage() {
       <img src={logo} alt="BTY logo" className="mb-8" />
       <h1 className="text-4xl font-bold mb-8 text-blue-500 text-2xl">Welcome to BTY</h1>
       <div className="w-2/3 text-xl mb-8 text-center">
-        Introducing our cutting-edge fitness tracker designed to help you stay fit and healthy even while you're at your desk! With a range of advanced features including personalized calisthenic workout plans and progress monitoring, our tracker is the perfect tool to keep you motivated and on track towards achieving your fitness goals. Say goodbye to sedentary habits and hello to a healthier, more active lifestyle with our desk-friendly fitness tracker.
+        Introducing our fitness tracker, <span className="font-bold text-blue-500">Better Than Yesterday</span>, designed to help you stay fit and healthy even while you're at your desk! With a variety of office-friendly exercises like seated leg raises, desk push-ups, and chair dips, this app makes it easy for users to incorporate physical activity into their workday. Say goodbye to sedentary habits and hello to a healthier, more active lifestyle with our desk-friendly fitness tracker.
       </div>
       <Link to="/authpage" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">GET STARTED</Link>
     </div>
