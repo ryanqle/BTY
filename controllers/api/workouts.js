191c77c2589ab5c171d3f3bac9fc6ed289403cd5
ryanqle <ryan.q.le@gmail.com>
2023-04-21 11:10:04 -0500

    Update formatting

--- controllers/api/workouts.js
+++ controllers/api/workouts.js
@@ -1,14 +1,14 @@
 const Workout = require('../../models/workout')
 
 module.exports = {
-    getWorkouts
-  }
+  getWorkouts
+}
 
-  async function getWorkouts(req,res) {
-    try {
-        const workouts = await Workout.find({}).populate('category')
-        res.json(workouts)
-      } catch (error) {
-        console.log(error)
-      }
+async function getWorkouts(req, res) {
+  try {
+    const workouts = await Workout.find({}).populate('category')
+    res.json(workouts)
+  } catch (error) {
+    console.log(error)
   }
+}
